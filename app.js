b32dd3d7a7118f5b0f733f916f0ada552790d548
Hugeelf <iam23db@yandex.ru>
2024-03-03 13:13:11 +0300

    ass animation and sorting for tasks (use unshift) - it might be better

--- app.js
+++ app.js
@@ -1,106 +1,147 @@
-const task = document.querySelector('.todo__input')
-const add = document.querySelector('.todo__add')
-const tasksList = document.querySelector(".todo__items")
+'use strict'
 
-let tasks = []
-if (localStorage.getItem('tasks')) {
-    tasks = JSON.parse(localStorage.getItem('tasks'))
-}
+const tasks =
+    (
+        localStorage.getItem('tasks')
+            ? JSON.parse(localStorage.getItem('tasks'))
+            : []
+    )
+
+const taskInput = document.querySelector('.todo__input')
+const tasksList = document.querySelector('.todo__items')
+const addButton = document.querySelector('.todo__add')
+
+
+
+//render existed tasks
 tasks.forEach(task => {
     renderTask(task)
 });
 
-add.addEventListener('click', addTask)
-task.addEventListener('keydown', function(event){
-    if(event.keyCode === 13){
-        addTask()
+//add task to tasks array with Enter button
+taskInput.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13 && taskInput.value) {
+        e.preventDefault()
+        addTask(taskInput.value)
+        taskInput.value = ''
+    }
+
+})
+
+//add task to tasks array with form button
+addButton.addEventListener('click', (e) => {
+    if (taskInput.value) {
+        e.preventDefault()
+        addTask(taskInput.value)
+        taskInput.value = ''
     }
 })
-tasksList.addEventListener('click', deleteTask)
-tasksList.addEventListener('click', doneTask)
 
-function addTask() {
-    if(!task.value){
-        return
-    }
-    const taskName = task.value
+// change todo status
+tasksList.addEventListener('click', (e) => {
+    toggleTaskStatusCss(e)
+    toggleTaskStatus(e)
+})
 
+function addTask(task) {
     const newTask = {
         id: Date.now(),
-        text: taskName,
-        done: false,
+        todoName: task,
+        done: false
     }
-
-    tasks.push(newTask)
-    renderTask(newTask)
-
+    tasks.unshift(newTask)
+    renderTask(newTask, 'afterbegin')
     saveToLocalStorage()
-    clearinput()
+    scrollToLastElement()
 }
 
-function deleteTask(event) {
-    if (event.target.dataset.action !== 'delete') {
-        return
-    }
-    const parentNode = event.target.closest('.todo__single_item')
-
-    const id = Number(parentNode.id)
-
-    const indexToDelete = tasks.findIndex((task) => task.id === id)
-    tasks.splice(indexToDelete, 1)
-
-    parentNode.remove()
-    saveToLocalStorage()
-}
-
-function doneTask(event) {
-
-    if (event.target.dataset.action !== 'completed') {
-        return
-    }
-
-    const parentNode = event.target.closest('.todo__single_item')
-
-    const id = Number(parentNode.id)
-
-    const task = tasks.find((task) => task.id === id)
-    task.done = !task.done
-
-    const taskTitle = parentNode.querySelector('.item__title')
-    taskTitle.classList.toggle('item__title--done')
-
-    const buttonRole = parentNode.querySelector('.todo__action')
-    buttonRole.classList.toggle('todo__action_restore')
-
-    saveToLocalStorage()
-
+function renderTask(task, where = 'beforeend') {
+    const taskNameClass = task.done ? "item__title item__title__done" : "item__title"
+    const buttonClass = task.done ? "todo__action_complete todo__action_restore" : "todo__action_complete"
+    // const taskStatusClass = task.done ? "todo__single_item__done" :  "todo__single_item"
+    tasksList.insertAdjacentHTML(where,
+        `
+    <li class="todo__single_item" id="${task.id}">
+        <span class="${taskNameClass}">
+            ${task.todoName}
+        </span>
+            <div class="item__buttons">
+                <button class="${buttonClass}"></button>
+                <button class="todo__action_delete"></button>
+             </div>
+     </li>
+`
+    )
 }
 
 function saveToLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
-function renderTask(task){
-    const cssTextClass = task.done ? "item__title item__title--done" : "item__title"
-    const cssButtonClass = task.done ? "todo__action todo__action_complete todo__action_restore" : "todo__action todo__action_complete"
-    const htmlTaskBlock = `
-    <li class="todo__single_item" id="${task.id}">
-    <span class="${cssTextClass}">${task.text}</span>
-    <div class="item__buttons">
-    <button
-      class="${cssButtonClass}"
-      data-action="completed"
-    ></button>
-    <button
-      class="todo__action todo__action_delete"
-      data-action="delete"
-    ></button>
-    </div>
-  </li>`
-
-    tasksList.insertAdjacentHTML('beforeend', htmlTaskBlock)
+function scrollToLastElement() {
+    tasksList.lastElementChild.scrollIntoView({
+        behavior: 'smooth'
+    })
 }
 
-function clearinput() {
-    task.value = ''
+function toggleTaskStatusCss(e) {
+    if (e.target.classList.contains('todo__action_complete')) {
+        e.target.classList.toggle('todo__action_restore')
+
+        e.target.closest('.todo__single_item')
+            .querySelector('.item__title').classList.toggle('item__title__done')
+        e.target.closest('.todo__single_item').classList.toggle('todo__single_item__done')
+    }
+    if (e.target.classList.contains('todo__action_delete')) {
+        e.target.closest('.todo__single_item').classList.add('todo__single_item__deleted')
+    }
+
+    // if(e.target.classList.contains('todo__action_restore')){
+    //     console.log(e.target)
+    // }
+}
+
+function toggleTaskStatus(e) {
+    const parentNode = e.target.closest('.todo__single_item')
+    const id = Number(parentNode.id)
+
+    if (e.target.classList.contains('todo__action_complete')
+        && e.target.classList.contains('todo__action_restore')) {
+        const taskForChangeStatus = tasks.find((task) => task.id === id)
+        taskForChangeStatus.done = !taskForChangeStatus.done
+
+        const indexOfTask = tasks.indexOf(taskForChangeStatus)
+        const task = tasks[indexOfTask]
+
+        tasks.splice(indexOfTask, 1)
+        tasks.push(task)
+        parentNode.addEventListener('animationend', () => {
+            parentNode.remove()
+        })
+        renderTask(task)
+    }
+    if (e.target.classList.contains('todo__action_delete')) {
+        const indexToDelete = tasks.findIndex((task) => task.id === id)
+        tasks.splice(indexToDelete, 1)
+        parentNode.addEventListener('animationend', () => {
+            parentNode.remove()
+        })
+    }
+
+    if((e.target.classList.contains('todo__action_complete')
+    && !e.target.classList.contains('todo__action_restore'))){
+        const taskForChangeStatus = tasks.find((task) => task.id === id)
+        taskForChangeStatus.done = !taskForChangeStatus.done
+
+        const indexOfTask = tasks.indexOf(taskForChangeStatus)
+        const task = tasks[indexOfTask]
+        tasks.splice(indexOfTask, 1)
+        tasks.unshift(task)
+        parentNode.addEventListener('animationend', () => {
+            parentNode.remove()
+        })
+        renderTask(task, 'afterbegin')
+        // console.log(parentNode)
+    }
+    saveToLocalStorage()
 }
